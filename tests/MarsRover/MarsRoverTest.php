4a84dd3c730c5fe806bb312d0259653d0314da1c
renato <renatogp13@gmail.com>
2023-11-13 11:34:17 +0100

    centralized creation of marsRover class to a function

--- tests/MarsRover/MarsRoverTest.php
+++ tests/MarsRover/MarsRoverTest.php
@@ -11,39 +11,43 @@ require_once 'src/MarsRover/MarsRover.php';
 
 class MarsRoverTest extends TestCase
 {
+    private function createMarsRover(): MarsRover
+    {
+        return new MarsRover();
+    }
     public function testInitialPosition()
     {
-        $marsRover = new MarsRover();
+        $marsRover = $this->createMarsRover();
         $this->assertEquals('0:0:N', $marsRover->execute(''));
     }
 
     public function testMoveForward()
     {
-        $marsRover = new MarsRover();
+        $marsRover = $this->createMarsRover();
         $this->assertEquals('0:1:N', $marsRover->execute('M'));
-        $marsRover = new MarsRover();
+        $marsRover = $this->createMarsRover();
         $this->assertEquals('0:2:N', $marsRover->execute('MM'));
     }
 
     public function testTurnLeft()
     {
-        $marsRover = new MarsRover();
+        $marsRover = $this->createMarsRover();
         $this->assertEquals('0:0:W', $marsRover->execute('L'));
     }
 
     public function testTurnRight()
     {
-        $marsRover = new MarsRover();
+        $marsRover = $this->createMarsRover();
         $this->assertEquals('0:0:E', $marsRover->execute('R'));
     }
 
     public function testComplexCommands()
     {
-        $marsRover = new MarsRover();
+        $marsRover = $this->createMarsRover();
         $this->assertEquals('2:1:N', $marsRover->execute('RMMLM'));
-        $marsRover = new MarsRover();
+        $marsRover = $this->createMarsRover();
         $this->assertEquals('2:3:N', $marsRover->execute('MMRMMLM'));
-        $marsRover = new MarsRover();
+        $marsRover = $this->createMarsRover();
         $this->assertEquals('0:0:N', $marsRover->execute('MMMMMMMMMM'));
     }
     public function testInvalidCommand()
@@ -51,7 +55,7 @@ class MarsRoverTest extends TestCase
         $this->expectException(InvalidArgumentException::class);
         $this->expectExceptionMessage("Invalid command: X");
 
-        $marsRover = new MarsRover();
+        $marsRover = $this->createMarsRover();
         $marsRover->execute('X');
     }
 
